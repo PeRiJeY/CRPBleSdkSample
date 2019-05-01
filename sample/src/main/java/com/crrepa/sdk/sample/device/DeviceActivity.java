8ac79ab45df0f806064ccdcc063b5f0d162c94fd
Juan German Arriaza Posada <juan.german.arriaza.posada@everis.com>
2019-05-01 12:16:21 +0200

    first commit

---
+++ sample/src/main/java/com/crrepa/sdk/sample/device/DeviceActivity.java
@@ -0,0 +1,321 @@
+package com.crrepa.sdk.sample.device;
+
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.crrepa.ble.CRPBleClient;
+import com.crrepa.ble.conn.CRPBleConnection;
+import com.crrepa.ble.conn.CRPBleDevice;
+import com.crrepa.ble.conn.bean.CRPAlarmClockInfo;
+import com.crrepa.ble.conn.bean.CRPFirmwareVersionInfo;
+import com.crrepa.ble.conn.bean.CRPFunctionInfo;
+import com.crrepa.ble.conn.bean.CRPFutureWeatherInfo;
+import com.crrepa.ble.conn.bean.CRPHeartRateInfo;
+import com.crrepa.ble.conn.bean.CRPMovementHeartRateInfo;
+import com.crrepa.ble.conn.bean.CRPSleepInfo;
+import com.crrepa.ble.conn.bean.CRPStepInfo;
+import com.crrepa.ble.conn.bean.CRPTodayWeatherInfo;
+import com.crrepa.ble.conn.bean.CRPUserInfo;
+import com.crrepa.ble.conn.bean.CRPWatchFaceLayoutInfo;
+import com.crrepa.ble.conn.callback.CRPDeviceAlarmClockCallback;
+import com.crrepa.ble.conn.callback.CRPDeviceBatteryCallback;
+import com.crrepa.ble.conn.callback.CRPDeviceBreathingLightCallback;
+import com.crrepa.ble.conn.callback.CRPDeviceDfuStatusCallback;
+import com.crrepa.ble.conn.callback.CRPDeviceDominantHandCallback;
+import com.crrepa.ble.conn.callback.CRPDeviceFirmwareVersionCallback;
+import com.crrepa.ble.conn.callback.CRPDeviceGoalStepCallback;
+import com.crrepa.ble.conn.callback.CRPDeviceLanguageCallback;
+import com.crrepa.ble.conn.callback.CRPDeviceMetricSystemCallback;
+import com.crrepa.ble.conn.callback.CRPDeviceNewFirmwareVersionCallback;
+import com.crrepa.ble.conn.callback.CRPDeviceOtherMessageCallback;
+import com.crrepa.ble.conn.callback.CRPDeviceQuickViewCallback;
+import com.crrepa.ble.conn.callback.CRPDeviceSedentaryReminderCallback;
+import com.crrepa.ble.conn.callback.CRPDeviceTimeSystemCallback;
+import com.crrepa.ble.conn.callback.CRPDeviceVersionCallback;
+import com.crrepa.ble.conn.callback.CRPDeviceWatchFaceLayoutCallback;
+import com.crrepa.ble.conn.callback.CRPDeviceWatchFacesCallback;
+import com.crrepa.ble.conn.listener.CRPBleConnectionStateListener;
+import com.crrepa.ble.conn.listener.CRPBleFirmwareUpgradeListener;
+import com.crrepa.ble.conn.listener.CRPBloodOxygenChangeListener;
+import com.crrepa.ble.conn.listener.CRPBloodPressureChangeListener;
+import com.crrepa.ble.conn.listener.CRPHeartRateChangeListener;
+import com.crrepa.ble.conn.listener.CRPPhoneOperationListener;
+import com.crrepa.ble.conn.listener.CRPSleepChangeListener;
+import com.crrepa.ble.conn.listener.CRPStepChangeListener;
+import com.crrepa.ble.conn.listener.CRPWatchFaceSwitchListener;
+import com.crrepa.ble.conn.type.CRPBleMessageType;
+import com.crrepa.ble.conn.type.CRPDeviceLanguageType;
+import com.crrepa.ble.conn.type.CRPDeviceVersionType;
+import com.crrepa.ble.conn.type.CRPDominantHandType;
+import com.crrepa.ble.conn.type.CRPFirmwareUpgradeType;
+import com.crrepa.ble.conn.type.CRPHeartRateType;
+import com.crrepa.ble.conn.type.CRPMetricSystemType;
+import com.crrepa.ble.conn.type.CRPPastTimeType;
+import com.crrepa.ble.conn.type.CRPTimeSystemType;
+import com.crrepa.ble.conn.type.CRPWatchFaceLayoutType;
+import com.crrepa.ble.conn.type.CRPWatchFacesType;
+import com.crrepa.ble.conn.type.CRPWeatherId;
+import com.crrepa.sdk.sample.R;
+import com.crrepa.sdk.sample.SampleApplication;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * Created by bill on 2017/5/15.
+ */
+
+public class DeviceActivity extends AppCompatActivity {
+    private static final String TAG = "DeviceActivity";
+    public static final String DEVICE_MACADDR = "device_macaddr";
+
+    ProgressDialog mProgressDialog;
+    CRPBleClient mBleClient;
+    CRPBleDevice mBleDevice;
+    CRPBleConnection mBleConnection;
+    boolean isUpgrade = false;
+    @BindView(R.id.tv_connect_state)
+    TextView tvConnectState;
+    @BindView(R.id.tv_heart_rate)
+    TextView tvHeartRate;
+    @BindView(R.id.tv_blood_pressure)
+    TextView tvBloodPressure;
+    @BindView(R.id.btn_ble_connect_state)
+    Button btnBleDisconnect;
+    @BindView(R.id.tv_blood_oxygen)
+    TextView tvBloodOxygen;
+
+    public long tiempo;
+    public String medidaActual = "";
+
+    private String bandFirmwareVersion;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device);
+        ButterKnife.bind(this);
+        initView();
+        mProgressDialog = new ProgressDialog(this);
+        String macAddr = getIntent().getStringExtra(DEVICE_MACADDR);
+        if (TextUtils.isEmpty(macAddr)) {
+            finish();
+            return;
+        }
+
+        mBleClient = SampleApplication.getBleClient(this);
+        mBleDevice = mBleClient.getBleDevice(macAddr);
+        if (mBleDevice != null && !mBleDevice.isConnected()) {
+            connect();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mBleDevice != null) {
+            mBleDevice.disconnect();
+        }
+    }
+
+    void initView() {
+
+    }
+
+    void connect() {
+        mProgressDialog.show();
+        mBleConnection = mBleDevice.connect();
+        mBleConnection.setConnectionStateListener(new CRPBleConnectionStateListener() {
+            @Override
+            public void onConnectionStateChange(int newState) {
+                Log.d(TAG, "onConnectionStateChange: " + newState);
+                int state = -1;
+                switch (newState) {
+                    case CRPBleConnectionStateListener.STATE_CONNECTED:
+                        state = R.string.state_connected;
+                        mProgressDialog.dismiss();
+                        updateTextView(btnBleDisconnect, getString(R.string.disconnect));
+                        testSet();
+                        break;
+                    case CRPBleConnectionStateListener.STATE_CONNECTING:
+                        state = R.string.state_connecting;
+                        break;
+                    case CRPBleConnectionStateListener.STATE_DISCONNECTED:
+                        state = R.string.state_disconnected;
+                        mProgressDialog.dismiss();
+                        updateTextView(btnBleDisconnect, getString(R.string.connect));
+                        break;
+                }
+                updateConnectState(state);
+            }
+        });
+
+
+        mBleConnection.setHeartRateChangeListener(mHeartRateChangListener);
+        mBleConnection.setBloodPressureChangeListener(mBloodPressureChangeListener);
+        mBleConnection.setBloodOxygenChangeListener(mBloodOxygenChangeListener);
+    }
+
+    private void testSet() {
+        mBleConnection.syncTime();
+        mBleConnection.queryPastHeartRate();
+        mBleConnection.syncSleep();
+    }
+
+
+    @OnClick(R.id.btn_ble_connect_state)
+    public void onConnectStateClick() {
+        if (mBleDevice.isConnected()) {
+            mBleDevice.disconnect();
+        } else {
+            mBleDevice.connect();
+        }
+    }
+
+
+    @OnClick({
+            R.id.btn_start_measure_all,
+            R.id.btn_stop_measure_all})
+    public void onViewClicked(View view) {
+        if (!mBleDevice.isConnected()) {
+            return;
+        }
+
+        tiempo = 0;
+        medidaActual="";
+
+        switch (view.getId()) {
+
+            case R.id.btn_start_measure_all:
+                tiempo = System.currentTimeMillis();
+                medidaActual="RATE";
+                mBleConnection.startMeasureOnceHeartRate();
+                int TIME = 30000; //5000 ms (5 Seconds)
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.d(TAG, "PRESS: " + (System.currentTimeMillis() - tiempo )/1000 +" S");
+                        medidaActual="PRESSURE";
+                        mBleConnection.startMeasureBloodPressure();
+                    }
+                }, TIME);
+                break;
+
+            case R.id.btn_stop_measure_all:
+                switch (medidaActual){
+                    case "RATE":
+                        mBleConnection.stopMeasureOnceHeartRate();
+                        break;
+                    case "PRESSURE":
+                        mBleConnection.stopMeasureBloodPressure();
+                        break;
+                    case "OXYGEN":
+                        mBleConnection.stopMeasureBloodOxygen();
+                        break;
+                }
+                break;
+        }
+    }
+
+
+    CRPHeartRateChangeListener mHeartRateChangListener = new CRPHeartRateChangeListener() {
+
+        @Override
+        public void onMeasuring(int rate) {
+            Log.d("PULSERA", "onMeasuring: " + rate);
+            updateTextView(tvHeartRate, String.format(getString(R.string.heart_rate), rate));
+            Log.d(TAG, "onConnectionStateChange: " + (System.currentTimeMillis() - tiempo )/1000 +" S");
+        }
+
+        @Override
+        public void onOnceMeasureComplete(int rate) {
+            Log.d("PULSERA", "onOnceMeasureComplete: " + rate);
+            Log.d(TAG, "onConnectionStateChange: " + (System.currentTimeMillis() - tiempo )/1000 +" S");
+        }
+
+
+        @Override
+        public void onMeasureComplete(CRPHeartRateInfo info) {
+            if (info != null && info.getMeasureData() != null) {
+                for (Integer integer : info.getMeasureData()) {
+                    Log.d("PULSERA", "onMeasureComplete: " + integer);
+
+                }
+            }
+        }
+
+        @Override
+        public void on24HourMeasureResult(CRPHeartRateInfo info) {
+            List<Integer> data = info.getMeasureData();
+            Log.d("PULSERA", "on24HourMeasureResult: " + data.size());
+        }
+
+        @Override
+        public void onMovementMeasureResult(List<CRPMovementHeartRateInfo> list) {
+            for (CRPMovementHeartRateInfo info : list) {
+                if (info != null) {
+                    Log.d("PULSERA", "onMovementMeasureResult: " + info.getStartTime());
+                }
+            }
+        }
+
+    };
+
+    CRPBloodPressureChangeListener mBloodPressureChangeListener = new CRPBloodPressureChangeListener() {
+        @Override
+        public void onBloodPressureChange(int sbp, int dbp) {
+            Log.d("PULSERA", "sbp: " + sbp + ",dbp: " + dbp);
+            updateTextView(tvBloodPressure,
+                    String.format(getString(R.string.blood_pressure), sbp, dbp));
+            mBleConnection.startMeasureBloodOxygen();
+            medidaActual="OXYGEN";
+        }
+
+    };
+
+    CRPBloodOxygenChangeListener mBloodOxygenChangeListener = new CRPBloodOxygenChangeListener() {
+        @Override
+        public void onBloodOxygenChange(int bloodOxygen) {
+            updateTextView(tvBloodOxygen,
+                    String.format(getString(R.string.blood_oxygen), bloodOxygen));
+        }
+    };
+
+
+
+
+
+
+    void updateConnectState(final int state) {
+        if (state < 0) {
+            return;
+        }
+        updateTextView(tvConnectState, getString(state));
+    }
+
+    void updateTextView(final TextView view, final String con) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                view.setText(con);
+            }
+        });
+    }
+
+}
---
+++ sample/src/main/java/com/crrepa/sdk/sample/scan/ScanActivity.java
@@ -0,0 +1,229 @@
+package com.crrepa.sdk.sample.scan;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.crrepa.ble.CRPBleClient;
+import com.crrepa.ble.conn.CRPBleDevice;
+import com.crrepa.ble.conn.listener.CRPBleFirmwareUpgradeListener;
+import com.crrepa.ble.scan.bean.CRPScanDevice;
+import com.crrepa.ble.scan.callback.CRPScanCallback;
+import com.crrepa.sdk.sample.R;
+import com.crrepa.sdk.sample.SampleApplication;
+import com.crrepa.sdk.sample.device.DeviceActivity;
+
+import java.util.List;
+import java.util.Set;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class ScanActivity extends AppCompatActivity {
+    private static final String TAG = "ScanActivity";
+    private static final int SCAN_PERIOD = 10 * 1000;
+    @BindView(R.id.scan_toggle_btn)
+    Button scanToggleBtn;
+    @BindView(R.id.scan_results)
+    RecyclerView scanResults;
+    @BindView(R.id.tv_firmware_fix_state)
+    TextView tvFirmwareFixState;
+
+    private CRPBleClient mBleClient;
+    private ScanResultsAdapter mResultsAdapter;
+    private boolean mScanState = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+        ButterKnife.bind(this);
+        mBleClient = SampleApplication.getBleClient(this);
+
+        configureResultList();
+
+        Set<BluetoothDevice> pairedDevices = BluetoothAdapter.getDefaultAdapter().getBondedDevices();
+
+        Log.i("ConnectedDevices: ", "Processing list");
+        if (pairedDevices.size() > 0) {
+            for (BluetoothDevice d: pairedDevices) {
+                String deviceName = d.getName();
+                String macAddress = d.getAddress();
+                Log.i("ConnectedDevices: ", "paired device: " + deviceName + " at " + macAddress);
+                // do what you need/want this these list items
+
+                if ("EA:0D:61:5A:16:B1".equals(macAddress)) {
+                    fastEnter(macAddress);
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        cancelScan();
+    }
+
+    @OnClick({R.id.scan_toggle_btn, R.id.btn_firmware_fix})
+    public void onViewClicked(View view) {
+        if (!mBleClient.isBluetoothEnable()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivity(enableBtIntent);
+            return;
+        }
+
+        switch (view.getId()) {
+            case R.id.scan_toggle_btn:
+                if (mScanState) {
+                    cancelScan();
+                } else {
+                    startScan();
+                }
+                break;
+            case R.id.btn_firmware_fix:
+                mBleClient.fixDeviceOfUpgrade("",
+                        "", mFirmwareUpgradeListener);
+                break;
+        }
+
+    }
+
+    private void startScan() {
+        boolean success = mBleClient.scanDevice(new CRPScanCallback() {
+            @Override
+            public void onScanning(final CRPScanDevice device) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mResultsAdapter.addScanResult(device);
+                    }
+                });
+            }
+
+            @Override
+            public void onScanComplete(List<CRPScanDevice> results) {
+                if (mScanState) {
+                    mScanState = false;
+                    updateButtonUIState();
+                }
+            }
+        }, SCAN_PERIOD);
+        if (success) {
+            mScanState = true;
+            updateButtonUIState();
+            mResultsAdapter.clearScanResults();
+        }
+    }
+
+    private void cancelScan() {
+        mBleClient.cancelScan();
+    }
+
+
+
+    private void configureResultList() {
+        scanResults.setHasFixedSize(true);
+        LinearLayoutManager recyclerLayoutManager = new LinearLayoutManager(this);
+        scanResults.setLayoutManager(recyclerLayoutManager);
+        mResultsAdapter = new ScanResultsAdapter();
+        scanResults.setAdapter(mResultsAdapter);
+        mResultsAdapter.setOnAdapterItemClickListener(new ScanResultsAdapter.OnAdapterItemClickListener() {
+            @Override
+            public void onAdapterViewClick(View view) {
+                final int childAdapterPosition = scanResults.getChildAdapterPosition(view);
+                final CRPScanDevice itemAtPosition = mResultsAdapter.getItemAtPosition(childAdapterPosition);
+                onAdapterItemClick(itemAtPosition);
+            }
+        });
+    }
+
+    private void fastEnter(String macAddress) {
+        mBleClient.cancelScan();
+
+        final Intent intent = new Intent(this, DeviceActivity.class);
+        intent.putExtra(DeviceActivity.DEVICE_MACADDR, macAddress);
+        startActivity(intent);
+    }
+
+    private void onAdapterItemClick(CRPScanDevice scanResults) {
+        final String macAddress = scanResults.getDevice().getAddress();
+        mBleClient.cancelScan();
+
+        final Intent intent = new Intent(this, DeviceActivity.class);
+        intent.putExtra(DeviceActivity.DEVICE_MACADDR, macAddress);
+        startActivity(intent);
+    }
+
+
+    private void updateButtonUIState() {
+        scanToggleBtn.setText(mScanState ? R.string.stop_scan : R.string.start_scan);
+    }
+
+    CRPBleFirmwareUpgradeListener mFirmwareUpgradeListener = new CRPBleFirmwareUpgradeListener() {
+        @Override
+        public void onFirmwareDownloadStarting() {
+            Log.d(TAG, "onFirmwareDownloadStarting");
+            updateTextView(tvFirmwareFixState, getString(R.string.dfu_status_download_starting));
+        }
+
+        @Override
+        public void onFirmwareDownloadComplete() {
+            Log.d(TAG, "onFirmwareDownloadComplete");
+            updateTextView(tvFirmwareFixState, getString(R.string.dfu_status_download_complete));
+        }
+
+        @Override
+        public void onUpgradeProgressStarting() {
+            Log.d(TAG, "onUpgradeProgressStarting");
+            updateTextView(tvFirmwareFixState, getString(R.string.dfu_status_starting));
+        }
+
+        @Override
+        public void onUpgradeProgressChanged(int percent, float speed) {
+            Log.d(TAG, "onUpgradeProgressChanged: " + percent);
+            String status = String.format(getString(R.string.dfu_status_uploading_part), percent);
+            updateTextView(tvFirmwareFixState, status);
+        }
+
+        @Override
+        public void onUpgradeCompleted() {
+            Log.d(TAG, "onUpgradeCompleted");
+            updateTextView(tvFirmwareFixState, getString(R.string.dfu_status_completed));
+        }
+
+        @Override
+        public void onUpgradeAborted() {
+            Log.d(TAG, "onUpgradeAborted");
+            updateTextView(tvFirmwareFixState, getString(R.string.dfu_status_aborted));
+        }
+
+        @Override
+        public void onError(int errorType, String message) {
+            Log.d(TAG, "onError: " + errorType);
+            updateTextView(tvFirmwareFixState, message);
+        }
+    };
+
+    void updateTextView(final TextView view, final String con) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                view.setText(con);
+            }
+        });
+    }
+
+
+}
